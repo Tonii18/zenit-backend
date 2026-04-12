d0477a903385af6029ce86dc8c4580a46a83cf20
Tonii18 <ajsanrosal@gmail.com>
2026-04-12 18:45:28 +0300

    Recipe controller and DTO done

---
+++ src/main/java/com/example/demo/models/RecipeRequestDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeRequestDTO {
+	
+	private int protein;
+	private int carb;
+	private int fiber;
+	private int calorie;
+
+}
